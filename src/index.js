5d47015726971cfd71a930ea35cf1a737a92ffa4
Oleksandr Mul <mulalex834@gmail.com>
2021-11-09 22:07:52 +0100

    Задание #4 - Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+import  "../index.css";
+import js_img from '../assets/js_image.png';
+
+console.log('Hello World');
+
+const i_love_js = document.createElement('h1')
+i_love_js.className = 'love_js'
+i_love_js.innerHTML = 'I love JavaScript'
+
+const image = document.createElement('img')
+image.className = 'js_img'
+image.src = js_img
+
+document.body.append(i_love_js, image)
+
